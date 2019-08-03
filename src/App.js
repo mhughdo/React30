ebfdd4cf6c9beda1637bf1575f00e8d48c5e5128
Manh Hung <hungdm1211@gmail.com>
2019-08-03 17:19:05 +0700

    with spinner HOC

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends React.Component {
     componentDidMount() {
         const {setCurrentUser} = this.props
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            console.log(userAuth)
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth)
-
                 userRef.onSnapshot(snapShot => {
                     setCurrentUser({
                         currentUser: {
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
-import App from './App'
 import {store, persistor} from './redux/store'
+import App from './App'
 
 ReactDOM.render(
     <Provider store={store}>
