208b8310e3b6ed92dd85abc4363c1306e6f2f747
Manh Hung <hungdm1211@gmail.com>
2019-08-21 11:49:19 +0700

    cartHidden & addItemToCart

---
+++ src/components/cart-dropdown/cart-dropdown.container.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {gql} from 'apollo-boost'
+import {Mutation, Query} from 'react-apollo'
+
+import CartDropdown from './cart-dropdown.component'
+
+const TOGGLE_CART_HIDDEN = gql`
+    mutation ToggleCartHidden {
+        toggleCartHidden @client
+    }
+`
+const GET_CART_ITEMS = gql`
+    {
+        cartItems @client
+    }
+`
+
+const CartDropdownContainer = () => (
+    <Mutation mutation={TOGGLE_CART_HIDDEN}>
+        {toggleCartHidden => (
+            <Query query={GET_CART_ITEMS}>
+                {({data: {cartItems}}) => <CartDropdown toggleCartHidden={toggleCartHidden} cartItems={cartItems} />}
+            </Query>
+        )}
+    </Mutation>
+)
+
+export default CartDropdownContainer
--- src/index.js
+++ src/index.js
@@ -12,16 +12,37 @@ import {store, persistor} from './redux/store'
 
 import './index.css'
 import App from './App'
+import {resolvers, typeDefs} from './graphql/resolvers'
 
+const httpLink = createHttpLink({
+    uri: 'https:crwn-clothing.com',
+})
 
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache,
+    resolvers,
+    typeDefs,
+})
+
+client.writeData({
+    data: {
+        cartHidden: true,
+        cartItems: [],
+    },
+})
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <PersistGate persistor={persistor}>
-                <App />
-            </PersistGate>
-        </BrowserRouter>
-    </Provider>,
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </BrowserRouter>
+        </Provider>
+    </ApolloProvider>,
     document.getElementById('root')
 )
